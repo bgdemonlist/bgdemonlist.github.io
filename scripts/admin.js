489d1fd5257dd4771abe066a15774993bdd62cb3
Stoyan Ivanov <101361817+soletki@users.noreply.github.com>
2025-10-04 21:29:00 +0300

    Update admin.js

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,11 +1,23 @@
-// Import the functions you need from the SDKs you need
+// Import Firebase SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
-import { getDatabase, set, ref, push, onValue, remove, query, orderByKey, orderByChild, orderByValue, get } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js"
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getDatabase,
+  set,
+  ref,
+  push,
+  onValue,
+  remove,
+  query,
+  orderByChild,
+  get
+} from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   databaseURL: "https://bulgarian-demonlist-default-rtdb.europe-west1.firebasedatabase.app/",
   apiKey: "AIzaSyBR-ImRkDyL_K3mwur6en4sXjj2WB9a-cs",
@@ -21,304 +33,227 @@ const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const auth = getAuth();
 
+// Navigation links
 const ul = document.getElementById("nav_links");
 
+// Handle authentication
 onAuthStateChanged(auth, (user) => {
   if (user) {
-    const uid = user.uid;
-    ul.innerHTML = 
-    `
-    <li><a href="admin.html">Admin</a></li>
-    <li><a href="leaderboard.html">Leaderboard</a></li>
-    `
-    console.log("signed in")
-  } else {
-    let password = prompt("Enter password")
-    signInWithEmailAndPassword(auth, "pishka@gmail.com", password)
-      ul.innerHTML = 
-      `
+    ul.innerHTML = `
+      <li><a href="admin.html">Admin</a></li>
       <li><a href="leaderboard.html">Leaderboard</a></li>
-      `
-      console.log("not signed in")
+    `;
+    console.log("signed in");
+  } else {
+    const password = prompt("Enter password");
+    signInWithEmailAndPassword(auth, "pishka@gmail.com", password);
+    ul.innerHTML = `
+      <li><a href="leaderboard.html">Leaderboard</a></li>
+    `;
+    console.log("not signed in");
   }
 });
 
-const levelSection = document.getElementById("levels-container")
-const levelSectionDiv = document.getElementById("levels-container-div")
-const levelSearch = document.getElementById("level-search")
-const plus = document.getElementById("plus")
-const minus = document.getElementById("minus")
-const blackScreen = document.getElementById("black-screen")
-const addPopup = document.getElementById("add-popup")
-const addPopupForm = document.getElementById("add-popup-bottom")
-const removePopupForm = document.getElementById("remove-popup-bottom")
-const removePopupCookies = document.getElementById("remove-popup-cookies")
-const removePopup = document.getElementById("remove-popup")
-const addPopupExit = document.getElementById("add-popup-close")
-const removePopupExit = document.getElementById("remove-popup-close")
+// UI elements
+const levelSectionDiv = document.getElementById("levels-container-div");
+const levelSearch = document.getElementById("level-search");
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const blackScreen = document.getElementById("black-screen");
+const addPopup = document.getElementById("add-popup");
+const removePopup = document.getElementById("remove-popup");
+const addPopupForm = document.getElementById("add-popup-bottom");
+const removePopupForm = document.getElementById("remove-popup-bottom");
+const addPopupExit = document.getElementById("add-popup-close");
+const removePopupExit = document.getElementById("remove-popup-close");
+
 let levelsList = [];
 let levelsAlreadyGenerated = false;
 
-onValue(query(ref(db, "levels"), orderByChild('pos')), snapshot=>{
-  levelSectionDiv.innerHTML = ""
-  document.getElementById("remove-popup-cookies-div").innerHTML = ""
-  snapshot.forEach(child => {
-    // levelsList = [];
-    let temp = child.val();
-    let levelContainer = document.createElement("div")
-    levelContainer.classList.add("level-container")
-    levelSectionDiv.append(levelContainer)
-    temp.element = levelContainer
+// Render levels ordered by position
+onValue(query(ref(db, "levels"), orderByChild("pos")), (snapshot) => {
+  levelSectionDiv.innerHTML = "";
+  document.getElementById("remove-popup-cookies-div").innerHTML = "";
 
-    let levelImage = document.createElement("img")
-    levelImage.classList.add("level-img")
-    let videoId = child.val().video.substr(17)
-    let image = "https://img.youtube.com/vi/" + videoId + "/maxresdefault.jpg"
-    levelImage.src = image
-    levelContainer.append(levelImage)
-    levelImage.addEventListener("click", function () {
-      window.open(child.val().video)
-    })
+  snapshot.forEach((child) => {
+    const data = child.val();
+    const levelContainer = document.createElement("div");
+    levelContainer.classList.add("level-container");
+    levelSectionDiv.append(levelContainer);
+    data.element = levelContainer;
 
-    let textContainer = document.createElement("div")
-    textContainer.classList.add("level-text-container")
-    levelContainer.append(textContainer)
+    // Level image
+    const levelImage = document.createElement("img");
+    levelImage.classList.add("level-img");
+    const videoId = data.video.substr(17);
+    levelImage.src = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
+    levelImage.addEventListener("click", () => window.open(data.video));
+    levelContainer.append(levelImage);
 
-    let levelName = document.createElement("h4")
-    levelName.innerHTML = "#" + child.val().pos + " - " + child.val().name
-    levelName.classList.add("level-name")
-    levelName.onclick = function () {
-      location.href = "admin-level.html?pos=" + child.val().pos
+    // Level text info
+    const textContainer = document.createElement("div");
+    textContainer.classList.add("level-text-container");
+    levelContainer.append(textContainer);
+
+    const levelName = document.createElement("h4");
+    levelName.textContent = `#${data.pos} - ${data.name}`;
+    levelName.classList.add("level-name");
+    levelName.onclick = () => {
+      location.href = `admin-level.html?pos=${data.pos}`;
+    };
+    textContainer.append(levelName);
+
+    const levelCreator = document.createElement("p");
+    levelCreator.textContent = data.creator;
+    levelCreator.classList.add("level-creator");
+    textContainer.append(levelCreator);
+
+    // Controls for moving levels up/down
+    const levelControls = document.createElement("div");
+    levelControls.classList.add("level-controls");
+    levelContainer.append(levelControls);
+
+    // Move up
+    if (data.pos > 1) {
+      const upArrow = document.createElement("i");
+      upArrow.classList.add("fa-solid", "fa-angle-up");
+      upArrow.addEventListener("click", () => moveLevelUp(data, snapshot));
+      levelControls.append(upArrow);
     }
-    textContainer.append(levelName)
 
-    let levelCreator = document.createElement("p")
-    levelCreator.innerHTML = child.val().creator
-    levelCreator.classList.add("level-creator")
-    textContainer.append(levelCreator)
-
-    let levelControls = document.createElement("div")
-    levelControls.classList.add("level-controls")
-    levelContainer.append(levelControls)
-
-    if(child.val().pos>1){
-      let upArrow = document.createElement("i")
-      upArrow.classList.add("fa-solid")
-      upArrow.classList.add("fa-angle-up")
-      upArrow.addEventListener("click", ()=>{
-        if(child.val().records){
-          set(ref(db, 'levels/' + child.val().name.toLowerCase()), {
-            name: child.val().name,
-            creator: child.val().creator,
-            video: child.val().video,
-            pos: child.val().pos-1,
-            records: child.val().records
-          })
-        }else{
-          set(ref(db, 'levels/' + child.val().name.toLowerCase()), {
-            name: child.val().name,
-            creator: child.val().creator,
-            video: child.val().video,
-            pos: child.val().pos-1
-          })
-        }
-        
-        if(levelsList[child.val().pos-2].records){
-          set(ref(db, 'levels/' + levelsList[child.val().pos-2].name.toLowerCase()), {
-            name: levelsList[child.val().pos-2].name,
-            creator: levelsList[child.val().pos-2].creator,
-            video: levelsList[child.val().pos-2].video,
-            pos: child.val().pos,
-            records: levelsList[child.val().pos-2].records
-          })
-        }else{
-          set(ref(db, 'levels/' + levelsList[child.val().pos-2].name.toLowerCase()), {
-            name: levelsList[child.val().pos-2].name,
-            creator: levelsList[child.val().pos-2].creator,
-            video: levelsList[child.val().pos-2].video,
-            pos: child.val().pos
-          })
-        }
-        
-        let templ = levelsList[child.val().pos-2];
-        levelsList[child.val().pos-2] = levelsList[child.val().pos-1];
-        levelsList[child.val().pos-1] = templ;
-      })
-      levelControls.append(upArrow)
+    // Move down
+    if (data.pos < Object.keys(snapshot.val()).length) {
+      const downArrow = document.createElement("i");
+      downArrow.classList.add("fa-solid", "fa-angle-down");
+      downArrow.addEventListener("click", () => moveLevelDown(data, snapshot));
+      levelControls.append(downArrow);
     }
-    
-    if(child.val().pos<Object.keys(snapshot.val()).length){
-      let downArrow = document.createElement("i")
-      downArrow.classList.add("fa-solid")
-      downArrow.classList.add("fa-angle-down")
-      downArrow.addEventListener("click", ()=>{
-        if(child.val().records){
-          set(ref(db, 'levels/' + child.val().name.toLowerCase()), {
-            name: child.val().name,
-            creator: child.val().creator,
-            video: child.val().video,
-            pos: child.val().pos+1,
-            records: child.val().records
-          })
-        }else{
-          set(ref(db, 'levels/' + child.val().name.toLowerCase()), {
-            name: child.val().name,
-            creator: child.val().creator,
-            video: child.val().video,
-            pos: child.val().pos+1
-          })
-        }
-        if(levelsList[child.val().pos].records){
-          set(ref(db, 'levels/' + levelsList[child.val().pos].name.toLowerCase()), {
-            name: levelsList[child.val().pos].name,
-            creator: levelsList[child.val().pos].creator,
-            video: levelsList[child.val().pos].video,
-            pos: child.val().pos,
-            records: levelsList[child.val().pos].video
-          })
-        }else{
-          set(ref(db, 'levels/' + levelsList[child.val().pos].name.toLowerCase()), {
-            name: levelsList[child.val().pos].name,
-            creator: levelsList[child.val().pos].creator,
-            video: levelsList[child.val().pos].video,
-            pos: child.val().pos
-          })
-        }
-        
-        let templ = levelsList[child.val().pos];
-        levelsList[child.val().pos] = levelsList[child.val().pos-1];
-        levelsList[child.val().pos-1] = templ;
-      })
-      levelControls.append(downArrow)
-    }
-    
-    
 
-    let cookie = document.createElement("h3");
-    cookie.addEventListener("click", ()=>{
-      document.getElementById("remove-popup-input").value = child.val().name;
-    })
-    cookie.innerHTML = "<h3>#" + child.val().pos + " - " + child.val().name + "</h3>"
-    temp.cookie = cookie;
-    if(!levelsAlreadyGenerated){
-      levelsList.push(temp);
-    }
-    document.getElementById("remove-popup-cookies-div").append(cookie)
-    
-  })
-})
+    // Remove cookie
+    const cookie = document.createElement("h3");
+    cookie.innerHTML = `#${data.pos} - ${data.name}`;
+    cookie.addEventListener("click", () => {
+      document.getElementById("remove-popup-input").value = data.name;
+    });
+    data.cookie = cookie;
 
+    if (!levelsAlreadyGenerated) levelsList.push(data);
+    document.getElementById("remove-popup-cookies-div").append(cookie);
+  });
+});
 
-levelSearch.addEventListener("input", e => {
-  const value = e.target.value.toLowerCase()
-  levelsList.forEach(level => {
-      const isVisible = level.name.toLowerCase().includes(value)
-      level.element.classList.toggle("hide", !isVisible)
-  })
-})
+// Search filter
+levelSearch.addEventListener("input", (e) => {
+  const value = e.target.value.toLowerCase();
+  levelsList.forEach((level) => {
+    const isVisible = level.name.toLowerCase().includes(value);
+    level.element.classList.toggle("hide", !isVisible);
+  });
+});
 
-plus.addEventListener("click", ()=>{
-  blackScreen.style.display = "flex"
-  addPopup.style.display = "flex"
-})
+// Popup handlers
+plus.addEventListener("click", () => {
+  blackScreen.style.display = "flex";
+  addPopup.style.display = "flex";
+});
 
-minus.addEventListener("click", ()=>{
-  blackScreen.style.display = "flex"
-  removePopup.style.display = "flex"
-})
+minus.addEventListener("click", () => {
+  blackScreen.style.display = "flex";
+  removePopup.style.display = "flex";
+});
 
-addPopupExit.addEventListener("click", ()=>{
-  blackScreen.style.display = "none"
-  addPopup.style.display = "none"
-})
+addPopupExit.addEventListener("click", closePopups);
+removePopupExit.addEventListener("click", closePopups);
 
-removePopupExit.addEventListener("click", ()=>{
-  blackScreen.style.display = "none"
-  removePopup.style.display = "none"
-})
+function closePopups() {
+  blackScreen.style.display = "none";
+  addPopup.style.display = "none";
+  removePopup.style.display = "none";
+}
 
-addPopupForm.addEventListener("submit", (event)=>{
-  
-  event.preventDefault()
+// Add new level
+addPopupForm.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-  const name = document.getElementById("add-popup-name").value
-  const creator = document.getElementById("add-popup-creator").value
-  const video = document.getElementById("add-popup-video").value
-  const pos = parseInt(document.getElementById("add-popup-pos").value)
+  const name = document.getElementById("add-popup-name").value;
+  const creator = document.getElementById("add-popup-creator").value;
+  const video = document.getElementById("add-popup-video").value;
+  const pos = parseInt(document.getElementById("add-popup-pos").value);
 
-  get(ref(db, "levels")).then(levels=>{
-    levels.forEach(level=>{
-      if(level.val().pos>=pos){
-        if(level.val().records){
-          set(ref(db, "levels/" + level.val().name.toLowerCase()), {
-            name: level.val().name,
-            creator: level.val().creator,
-            video: level.val().video,
-            pos: level.val().pos+1,
-            records: level.val().records
-          })
-        }else{
-          set(ref(db, "levels/" + level.val().name.toLowerCase()), {
-            name: level.val().name,
-            creator: level.val().creator,
-            video: level.val().video,
-            pos: level.val().pos+1
-          })
-        }
-        
+  get(ref(db, "levels")).then((levels) => {
+    levels.forEach((level) => {
+      const data = level.val();
+      if (data.pos >= pos) {
+        set(ref(db, `levels/${data.name.toLowerCase()}`), {
+          ...data,
+          pos: data.pos + 1
+        });
       }
-    })
-  })
+    });
+  });
 
-  set(ref(db, "levels/" + name.toLowerCase()), {
-    name: name,
-    creator: creator,
-    video: video,
-    pos: pos
-  })
-})
+  set(ref(db, `levels/${name.toLowerCase()}`), {
+    name,
+    creator,
+    video,
+    pos
+  });
+});
 
-document.getElementById("remove-popup-input").addEventListener("keyup", e=>{
-  const value = e.target.value.toLowerCase()
-  levelsList.forEach(level => {
-      const isVisible = level.name.toLowerCase().includes(value)
-      level.cookie.classList.toggle("hide", !isVisible)
-  })
-})
+// Remove level
+document.getElementById("remove-popup-input").addEventListener("keyup", (e) => {
+  const value = e.target.value.toLowerCase();
+  levelsList.forEach((level) => {
+    const isVisible = level.name.toLowerCase().includes(value);
+    level.cookie.classList.toggle("hide", !isVisible);
+  });
+});
 
-removePopupForm.addEventListener("submit", (event)=>{
+removePopupForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const name = document.getElementById("remove-popup-input").value;
 
-  event.preventDefault()
-
-  const name = document.getElementById("remove-popup-input").value
-
-  get(ref(db, "levels/" + name.toLowerCase())).then((curr)=>{
-    get(ref(db, "levels")).then(levels=>{
-      levels.forEach(level=>{
-        if(level.val().pos>curr.val().pos){
-          if(level.val().records){
-            set(ref(db, "levels/" + level.val().name.toLowerCase()), {
-              name: level.val().name,
-              creator: level.val().creator,
-              video: level.val().video,
-              pos: level.val().pos-1,
-              records: level.val().records
-            })
-          }else{
-            set(ref(db, "levels/" + level.val().name.toLowerCase()), {
-              name: level.val().name,
-              creator: level.val().creator,
-              video: level.val().video,
-              pos: level.val().pos-1
-            })
-          }
-          
+  get(ref(db, `levels/${name.toLowerCase()}`)).then((curr) => {
+    get(ref(db, "levels")).then((levels) => {
+      levels.forEach((level) => {
+        const data = level.val();
+        if (data.pos > curr.val().pos) {
+          set(ref(db, `levels/${data.name.toLowerCase()}`), {
+            ...data,
+            pos: data.pos - 1
+          });
         }
-      })
-    })
-  }).then(remove(ref(db, 'levels/' + name.toLowerCase())))
+      });
+    });
+  }).then(() => remove(ref(db, `levels/${name.toLowerCase()}`)));
+});
 
-  
+// Helper functions for moving levels
+function moveLevelUp(level, snapshot) {
+  const prev = levelsList[level.pos - 2];
+  swapPositions(level, prev, -1);
+}
 
-  
-})
+function moveLevelDown(level, snapshot) {
+  const next = levelsList[level.pos];
+  swapPositions(level, next, +1);
+}
+
+function swapPositions(curr, other, offset) {
+  // Update current
+  set(ref(db, `levels/${curr.name.toLowerCase()}`), {
+    ...curr,
+    pos: curr.pos + offset
+  });
+
+  // Update swapped
+  set(ref(db, `levels/${other.name.toLowerCase()}`), {
+    ...other,
+    pos: curr.pos
+  });
+
+  // Swap locally
+  const temp = levelsList[curr.pos - 1 + offset];
+  levelsList[curr.pos - 1 + offset] = levelsList[curr.pos - 1];
+  levelsList[curr.pos - 1] = temp;
+}
